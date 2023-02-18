752b6b9e6afd3dd606435d395640cc11a9fa79f3
Nimrook <kramleknod@gmail.com>
2023-02-18 18:59:34 +0100

    Fixed interlinking

--- js/word-counter.js
+++ js/word-counter.js
@@ -2,15 +2,11 @@ $(document).ready(function(){
 
   $('#input').keyup(function(){
     var text = $('#input').val();
-  });
-
-  $('#input').keyup(function(){
-    var text = $('#input').val();
+    
+    // Characters
     $('#charcounter').text(text.length);
-  });
 
-  $('#input').keyup(function(){
-    var text = $('#input').val();
+    // Characters without space
     var spaceCounter = 0;
     for(var i = 0; i < text.length; i++){
       if(text[i] == ' '){
@@ -18,15 +14,20 @@ $(document).ready(function(){
       }
     }
     $('#charcounternospace').text(text.length-spaceCounter);
-  });
 
-  $('#input').keyup(function(){
-    var text = $('#input').val();
+    // Sentences
     var sentenceCounter = 0;
     if (text.match(/[.!?](\s|\n)/g) !== null){
       sentenceCounter = text.match(/[.!?](\s|\n)/g).length;
     }
     $('#sentencecounter').text(sentenceCounter);
+
+    // Paragraphs
+    var paragraphCounter = 0;
+    if (text.match(/[\r\n]+/g) !== null){
+      paragraphCounter = text.match(/[\r\n]+/g).length;
+    }
+    $('#paragraphcounter').text(paragraphCounter);
   });
   
 });
